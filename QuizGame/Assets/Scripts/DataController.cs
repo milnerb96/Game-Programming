69b2f967dabda289ad199c75901e6d93081a30d4
milnerb96 <blaze_zac@yahoo.com>
2019-02-09 14:21:41 -0600

    Initial Commit
    Contains QuizGame folder and the Zoom recording explaining the game.

---
+++ QuizGame/Assets/Scripts/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class DataController : MonoBehaviour
+{
+
+    private RoundData[] allRoundData;
+    private string gameDataFileName = "data.json";
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        DontDestroyOnLoad(gameObject);
+        LoadGameData();
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            allRoundData = loadedData.allRoundData;
+        }
+
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+}
